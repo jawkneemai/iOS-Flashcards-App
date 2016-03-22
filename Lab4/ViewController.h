186a066a8b4baf2739504b3d1686b2d1795ffffa
Johnny <johnathm@usc.edu>
2016-03-22 14:43:40 -0700

    Initial Commit

---
+++ Lab4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab4
+//
+//  Created by Johnathan Mai on 3/22/16.
+//  Copyright © 2016 Johnathan Mai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
